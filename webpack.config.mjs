f57568daf4f3c87bd60e265cda211e58b3e63823
ojagggyo <ojagggyo@gmail.com>
2026-04-01 00:26:39 +0900

    m

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,5 +1,6 @@
 import path from 'path';
 import nodeExternals from 'webpack-node-externals';
+import 'regenerator-runtime/runtime.js';
 
 // ESモジュール環境で __dirname の代わりに import.meta.url を使ってパスを計算
 const __dirname = new URL('.', import.meta.url).pathname;
