3a16790e02cb713201332b12e9564cce76faba2b
ojagggyo <ojagggyo@gmail.com>
2026-04-10 23:00:47 +0900

    m

---
+++ webpack.config.OK.js
@@ -0,0 +1,21 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    target: 'web', // これを追加
+    // Uncaught (in promise) ReferenceError: regeneratorRuntime is not defined
+    entry: ['regenerator-runtime/runtime.js', './src/app.js'],
+    // 出力の設定
+    output: {
+        // 出力するファイル名
+        filename: 'bundle.js',
+        // 出力先のパス（絶対パスを指定する必要がある）
+        path: path.join(__dirname, 'public')
+    },
+
+    externals: {
+        'node:undici': 'commonjs node:undici',
+        '@steemit/steem-js': 'steem' // 外部依存として設定
+    },
+};
