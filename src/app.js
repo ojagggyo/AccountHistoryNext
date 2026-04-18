ace513a29188953e1e589bd188b6ff435f3618fa
ojagggyo <ojagggyo@gmail.com>
2026-04-18 21:10:02 +0900

    m

--- src/app.js
+++ src/app.js
@@ -1653,6 +1653,12 @@ window.showTooltip_post = async (e) => {
     let author = e.target.getAttribute('data-author');
     let permlink = e.target.getAttribute('data-permlink');
 
+		// ツールチップにローディングメッセージを表示
+    tooltip.innerHTML = "Loading...";
+    tooltip.style.top = e.pageY + 10 + 'px';  // 位置にpx単位を追加
+    tooltip.style.left = e.pageX + 10 + 'px';  // 位置にpx単位を追加
+    tooltip.style.display = "block";
+
     // APIを呼び出し、投稿のメタデータを取得
     steem.api.callAsync('condenser_api.get_content', [author, permlink])
     .then(result => {
@@ -1662,11 +1668,7 @@ window.showTooltip_post = async (e) => {
         console.error("Error:", error);
     });
 
-	// ツールチップにローディングメッセージを表示
-    tooltip.innerHTML = "Loading...";
-    tooltip.style.top = e.pageY + 10 + 'px';  // 位置にpx単位を追加
-    tooltip.style.left = e.pageX + 10 + 'px';  // 位置にpx単位を追加
-    tooltip.style.display = "block";
+
 }
 
 function createTooltip(e, result, author) {
@@ -1687,10 +1689,15 @@ function createTooltip(e, result, author) {
     }
 
     // ツールチップの位置を調整（画面からはみ出さないように）
+	let tooltip_w = parseInt(window.getComputedStyle(tooltip).width);
     let document_w = document.documentElement.clientWidth;
     let document_h = document.documentElement.clientHeight;
     let tooltipWidth = tooltip.offsetWidth;
     let tooltipHeight = tooltip.offsetHeight;
+
+	console.log(tooltip_w);
+	console.log(`${document_w} ${document_h}`);
+	console.log(`${tooltipWidth} ${tooltipHeight}`);
 
     let tooltipX = e.pageX + 10;
     let tooltipY = e.pageY + 10;
