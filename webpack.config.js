b9272009e17d3145ccd38d4a554caca35064f37f
ojagggyo <ojagggyo@gmail.com>
2026-07-12 21:29:40 +0900

    m

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     // 出力の設定
     output: {
         // 出力するファイル名
-        filename: 'bundle.2.js',
+        filename: 'bundle.3.js',
         // 出力先のパス（絶対パスを指定する必要がある）
         path: path.join(__dirname, 'public')
     },
